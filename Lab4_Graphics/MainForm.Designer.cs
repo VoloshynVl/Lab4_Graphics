87633859859e89d76bbbd7c1079d91f760cea054
VladX <147607081+VoloshynVl@users.noreply.github.com>
2025-06-02 01:54:26 +0300

    Add files via upload

---
+++ Lab4_Graphics/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Lab4_Graphics
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.ComboBox shapeComboBox;
+        private System.Windows.Forms.ComboBox colorComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.shapeComboBox = new System.Windows.Forms.ComboBox();
+            this.colorComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // drawPanel
+            this.drawPanel.BackColor = System.Drawing.Color.White;
+            this.drawPanel.Location = new System.Drawing.Point(12, 50);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(760, 400);
+            this.drawPanel.TabIndex = 0;
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawPanel_Paint);
+            this.drawPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseClick);
+
+            // shapeComboBox
+            this.shapeComboBox.FormattingEnabled = true;
+            this.shapeComboBox.Items.AddRange(new object[] {
+            "Еліпс",
+            "Коло",
+            "Прямокутник",
+            "Квадрат",
+            "Лінія"});
+            this.shapeComboBox.Location = new System.Drawing.Point(100, 12);
+            this.shapeComboBox.Name = "shapeComboBox";
+            this.shapeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.shapeComboBox.TabIndex = 1;
+            this.shapeComboBox.SelectedIndexChanged += new System.EventHandler(this.shapeComboBox_SelectedIndexChanged);
+
+            // colorComboBox
+            this.colorComboBox.FormattingEnabled = true;
+            this.colorComboBox.Items.AddRange(new object[] {
+            "Червоний",
+            "Зелений",
+            "Синій",
+            "Жовтий",
+            "Фіолетовий"
+            });
+            this.colorComboBox.Location = new System.Drawing.Point(320, 12);
+            this.colorComboBox.Name = "colorComboBox";
+            this.colorComboBox.Size = new System.Drawing.Size(121, 21);
+            this.colorComboBox.TabIndex = 2;
+            this.colorComboBox.SelectedIndexChanged += new System.EventHandler(this.colorComboBox_SelectedIndexChanged);
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Text = "Тип фігури:";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Text = "Колір фігури:";
+
+            // MainForm
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorComboBox);
+            this.Controls.Add(this.shapeComboBox);
+            this.Controls.Add(this.drawPanel);
+            this.Name = "MainForm";
+            this.Text = "Lab 4 – Graphics in WinForms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Lab4_Graphics/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab4_Graphics
+{
+    public partial class MainForm : Form
+    {
+        private string selectedShape = "Прямокутник";
+        private Color selectedColor = Color.Red;
+        private List<(string shape, Point location, Color color)> shapes = new();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            shapeComboBox.SelectedIndex = 0;
+            colorComboBox.SelectedIndex = 0;
+        }
+
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            foreach (var shape in shapes)
+            {
+                SolidBrush brush = new SolidBrush(shape.color);
+                Pen pen = new Pen(shape.color, 2);
+                Random rnd = new Random();
+                int w = rnd.Next(50, 100);
+                int h = rnd.Next(50, 100);
+
+                switch (shape.shape)
+                {
+                    case "Еліпс":
+                        g.FillEllipse(brush, shape.location.X, shape.location.Y, w, h);
+                        break;
+                    case "Коло":
+                        int r = rnd.Next(50, 100);
+                        g.FillEllipse(brush, shape.location.X, shape.location.Y, r, r);
+                        break;
+                    case "Прямокутник":
+                        g.FillRectangle(brush, shape.location.X, shape.location.Y, w, h);
+                        break;
+                    case "Квадрат":
+                        int s = rnd.Next(50, 100);
+                        g.FillRectangle(brush, shape.location.X, shape.location.Y, s, s);
+                        break;
+                    case "Лінія":
+                        g.DrawLine(pen, shape.location.X, shape.location.Y,
+                            shape.location.X + rnd.Next(30, 100),
+                            shape.location.Y + rnd.Next(-50, 50));
+                        break;
+                }
+            }
+
+            // логотип
+            Font logoFont = new Font("Segoe UI", 16, FontStyle.Bold);
+            g.DrawString("МійЛоготип", logoFont, Brushes.DarkBlue, 10, 10);
+            g.FillEllipse(Brushes.Orange, 160, 12, 20, 20);
+        }
+
+        private void drawPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            shapes.Add((selectedShape, e.Location, selectedColor));
+            drawPanel.Invalidate();
+        }
+
+        private void shapeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedShape = shapeComboBox.SelectedItem.ToString();
+        }
+
+        private void colorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string colorName = colorComboBox.SelectedItem.ToString();
+
+            switch (colorName)
+            {
+                case "Червоний":
+                    selectedColor = Color.Red;
+                    break;
+                case "Зелений":
+                    selectedColor = Color.Green;
+                    break;
+                case "Синій":
+                    selectedColor = Color.Blue;
+                    break;
+                case "Жовтий":
+                    selectedColor = Color.Yellow;
+                    break;
+                case "Фіолетовий":
+                    selectedColor = Color.Purple;
+                    break;
+                default:
+                    selectedColor = Color.Black;
+                    break;
+            }
+        }
+    }
+}
